d20e32b2ca8d500a9c70f9215c9a7693d1dc730a
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-04-29 23:24:35 +0600

    create node for list

---
+++ createNode.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <error.h>
+
+typedef struct node Node;
+
+struct node
+{
+    int data;
+    Node *next;
+};
+
+Node *head;
+
+Node *create_node(int item, Node *next)
+{
+    Node *new_node = (Node *)malloc(sizeof(Node));
+
+    if (new_node == NULL)
+    {
+        printf("Error! Could not create A new node\n");
+        exit(1);
+    }
+
+    new_node->data = item;
+    new_node->next = next;
+
+    return new_node;
+}
+
+int main()
+{
+    Node *n;
+    n = create_node(10, NULL);
+    printf("data = %d\n", n->data);
+    n = create_node(15, NULL);
+    printf("data = %d\n", n->data);
+    n = create_node(18, NULL);
+    printf("data = %d\n", n->data);
+    return 0;
+}
