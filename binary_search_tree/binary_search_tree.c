2aaad8df1951517fcfb04ce41b863b8d0152624a
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-05-05 16:26:49 +0600

    bst delete functionality usage

--- binary_search_tree/binary_search_tree.c
+++ binary_search_tree/binary_search_tree.c
@@ -161,19 +161,49 @@ Node *bst_search(Node *root, int item)
     return node;
 }
 
-
-
-Node *bst_transplant(Node *root, Node *current_node, Node *new_node){
-    if(current_node == root){
+Node *bst_transplant(Node *root, Node *current_node, Node *new_node)
+{
+    if (current_node == root)
+    {
         root = new_node;
-    }else if(current_node == current_node->parent->left){
-        add_left_child(current_node->parent, new_node);        
-    }else{
+    }
+    else if (current_node == current_node->parent->left)
+    {
+        add_left_child(current_node->parent, new_node);
+    }
+    else
+    {
         add_right_child(current_node->parent, new_node);
     }
     return root;
 }
 
+Node *bst_delete(Node *root, Node *node)
+{
+    Node *smallest_node;
+
+    if (node->left == NULL)
+    {
+        root = bst_transplant(root, node, node->right);
+    }
+    else if (node->right == NULL)
+    {
+        root = bst_transplant(root, node, node->left);
+    }
+    else
+    {
+        smallest_node = bst_minimum(node->right);
+        if (smallest_node->parent != node)
+        {
+            root = bst_transplant(root, smallest_node, smallest_node->right);
+            add_right_child(smallest_node, node->right);
+        }
+        root = bst_transplant(root, node, smallest_node);
+        add_left_child(smallest_node, node->left);
+    }
+    free(node);
+    return root;
+};
 
 int main()
 {
