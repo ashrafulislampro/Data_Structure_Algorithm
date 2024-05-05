570df01512b42cee572da2a91a3964d921b45b53
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-05-05 15:38:52 +0600

    bst transplant usage

--- binary_search_tree/binary_search_tree.c
+++ binary_search_tree/binary_search_tree.c
@@ -161,6 +161,20 @@ Node *bst_search(Node *root, int item)
     return node;
 }
 
+
+
+Node *bst_transplant(Node *root, Node *current_node, Node *new_node){
+    if(current_node == root){
+        root = new_node;
+    }else if(current_node == current_node->parent->left){
+        add_left_child(current_node->parent, new_node);        
+    }else{
+        add_right_child(current_node->parent, new_node);
+    }
+    return root;
+}
+
+
 int main()
 {
     Node *root = create_bst();
